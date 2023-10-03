64a3bbf3a6deb318322c054d291b3aa019b6b221
Aleksandra Kudlinska <olakudlinska@gmail.com>
2023-10-03 14:19:29 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!"); // [3]
+    {System.out.println("Aleksandra"); }
+        {System.out.println(26); }
+        {System.out.println('K'); }}
+}
